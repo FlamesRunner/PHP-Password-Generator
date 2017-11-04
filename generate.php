a32a1881bc27ff19bc08c239d1321f13918a5bd1
Andrew H <FlamesRunner@users.noreply.github.com>
2017-11-04 18:52:33 -0400

    Code fixxes

--- generate.php
+++ generate.php
@@ -1,24 +1,25 @@
 <?php
 function generateRandomPassword($characters = 7) {
-	$words = file_get_contents('wordlist.txt');
-	$symbols = array('@', '&', '!', '#', '$', '%');
-	$wordList = explode("\n", $words);
+        $words = file_get_contents('wordlist.txt');
+        $symbols = array('@', '&', '!', '#', '$', '%');
+        $wordList = explode("\n", $words);
         for ($i = 0; $i <= $characters; $i++){
-		if (strlen($password) >= $characters) break;
-	        $choice = rand(0, count($wordList));
-		$passwordPhrase .= $wordList[$choice];
-		if (rand(0, 100) > 50) $password .= strtoupper(substr($wordList[$choice], 0, 1));
-		if (rand(0, 100) > 80){
-			$symbolID = rand(0, count($symbols));
-			$password .= $symbols[$symbolID];
-			$passwordPhrase .= $symbols[$symbolID] . ' ';
-		}
-	}
-	$values = array('password' => $password, 'words' => $passwordPhrase);
-	return $values;
+                $password = trim($password);
+                if (strlen($password) >= $characters) break;
+                $choice = rand(0, count($wordList));
+                $passwordPhrase .= $wordList[$choice];
+                if (rand(0, 100) > 50) { $password .= strtoupper(substr($wordList[$choice], 0, 1)); } else { $password .= substr($wordList[$choice], 0, 1); }
+                if (rand(0, 100) > 80) {
+                        $symbolID = rand(0, count($symbols)-1);
+                        $password .= $symbols[$symbolID];
+                        $passwordPhrase .= $symbols[$symbolID] . ' ';
+                }
+        }
+        $values = array('password' => $password, 'words' => $passwordPhrase);
+        return $values;
 }
 
 $length = $_POST['len'];
-if (!isset($_POST['len']) || !is_numeric($_POST['len']) || $_POST['len'] > 64) $length = 7;
+if (!isset($_POST['len']) && !is_numeric($length) && $length > 64) $length = 7;
 echo json_encode(generateRandomPassword($length));
 ?>
